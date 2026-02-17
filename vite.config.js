6137eb1ef40b45a5adf89e3e590d0d9c75ce0e92
YasamSriCharan <2500030149@kluniversity.in>
2026-02-17 10:05:08 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/employeepage/", // YourRepoName
+  base: "/USER-INFORMATION-DASHBOARD/", // YourRepoName
 })
